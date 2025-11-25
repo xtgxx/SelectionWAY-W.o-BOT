e4f8dc11de3a34662ca71acfe461f224dbc5d56f
xtgxx <joket865@gmail.com>
2025-11-25 22:09:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from flask import Flask #𓍯𝙎𝙪𝙟𝙖𝙡⚝
 from telebot.apihelper import ApiTelegramException #𓍯𝙎𝙪𝙟𝙖𝙡⚝
 
 # ---------------- CONFIG ---------------- #𓍯𝙎𝙪𝙟𝙖𝙡⚝
-BOT_TOKEN = "8294450252:AAHJ0MlSy90RLZRvN7aAEncfPzRCJ6xLL1E" # <-- REPLACE with your Bot token #𓍯𝙎𝙪𝙟𝙖𝙡⚝
+BOT_TOKEN = "8294450252:AAEBj5jrMNAdwyRyhfF9hGuQBTr9IkExmGk" # <-- REPLACE with your Bot token #𓍯𝙎𝙪𝙟𝙖𝙡⚝
 BASE_URL = "https://backend.multistreaming.site/api" #𓍯𝙎𝙪𝙟𝙖𝙡⚝
 USER_ID_FOR_ACTIVE = "1448640" #𓍯𝙎𝙪𝙟𝙖𝙡⚝
 BASE_HEADERS = { #𓍯𝙎𝙪𝙟𝙖𝙡⚝
